beee4b645e6ac41f68d23750cac0c6856d4d04d9
user15213 <resomakar@gmail.com>
2025-03-30 17:35:39 +0500

    add vercel.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 import path from 'path';
+import url from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+// Получаем путь к текущей директории
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -19,8 +25,14 @@ export default {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+    open: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
 };
