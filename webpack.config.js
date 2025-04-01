b3cba0669a585d1ee21e0c139d2839c04bf8ce24
user15213 <resomakar@gmail.com>
2025-04-01 14:05:57 +0500

    add dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default {
-  mode: 'production',
+  mode: process.env.NODE_ENV || 'production',
   module: {
     rules: [
       {
@@ -32,19 +32,18 @@ export default {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
-      inject: true,
-      publicPath: '/',
+      template: 'index.html', // Ваш исходный файл HTML
+      inject: true, // Вставка в HTML
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: '[name].[contenthash].css', // Указываем хэш
     }),
   ],
   output: {
     clean: true,
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'), // Путь для сборки
+    filename: '[name].[contenthash].js', // Имя JS файла
+    publicPath: '', // Абсолютный путь
   },
   devServer: {
     static: path.join(__dirname, 'dist'),
