d9ddfcc69d584766c77cc471ece359f532ff33f2
user15213 <resomakar@gmail.com>
2025-04-06 15:50:47 +0500

    fix app render

--- src/app.js
+++ src/app.js
@@ -9,23 +9,23 @@ import parseRSS from './utils/parser.js';
 
 const getAllOriginsResponse = (url) => {
   const allOriginsLink = 'https://allorigins.hexlet.app/get';
+
   const workingUrl = new URL(allOriginsLink);
+
   workingUrl.searchParams.set('disableCache', 'true');
   workingUrl.searchParams.set('url', url);
+
   return axios.get(workingUrl);
 };
 
 const getHttpContents = (url) =>
   getAllOriginsResponse(url)
     .then((response) => {
-      if (!response.data || !response.data.contents) {
-        throw new Error('InvalidResponse');
-      }
-      return response.data.contents;
+      const responseData = response.data.contents;
+      return responseData;
     })
-    .catch((error) => {
-      console.error('Error fetching HTTP contents:', error);
-      throw new Error('networkError', { cause: error });
+    .catch(() => {
+      throw new Error('networkError');
     });
 
 const addPosts = (feedId, items, state) => {
@@ -34,51 +34,46 @@ const addPosts = (feedId, items, state) => {
     id: uniqueId(),
     ...item,
   }));
-  // eslint-disable-next-line no-param-reassign
   state.posts = posts.concat(state.posts);
 };
 
-const trackUpdates = (state, timeout = 5000) => {
+const trackUpdates = (feedIds, state, timeout = 5000) => {
   const inner = () => {
     const promises = state.feeds.map((feed) =>
       getHttpContents(feed.link)
-        .then((data) => {
-          const parsedRSS = parseRSS(data);
-          return parsedRSS;
-        })
+        .then(parseRSS)
         .catch((error) => ({ error }))
     );
+
     Promise.allSettled(promises)
-      .then((results) => {
+      .then((results) =>
         results.forEach((result, index) => {
           const parsedRSS = result.status === 'fulfilled' ? result.value : null;
           const feedId = state.feeds[index].id;
-          if (!parsedRSS) {
-            return;
-          }
+
+          if (!parsedRSS) return;
+
           const postsUrls = state.posts
             .filter((post) => feedId === post.feedId)
             .map(({ link }) => link);
           const newItems = parsedRSS.items.filter(
             ({ link }) => !postsUrls.includes(link)
           );
+
           if (newItems.length > 0) {
             addPosts(feedId, newItems, state);
           }
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .then(() => {
-        setTimeout(inner, timeout);
-      });
+        })
+      )
+      .catch((error) => console.error(error))
+      .then(() => setTimeout(inner, timeout));
   };
   setTimeout(inner, timeout);
 };
 
 export default () => {
   const defaultLanguage = 'ru';
+
   const i18nInstance = i18next.createInstance();
 
   setLocale({
@@ -115,9 +110,11 @@ export default () => {
           url: '',
           error: '',
         },
+
         feeds: [],
         posts: [],
         seenIds: new Set(),
+
         modal: {
           title: '',
           description: '',
@@ -130,31 +127,39 @@ export default () => {
         render(elements, initialState, i18nInstance)
       );
 
-      trackUpdates(state);
+      trackUpdates(state.feeds, state);
 
       elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
+
         state.form.error = '';
-        state.form.state = 'sending';
+
         const urlsList = state.feeds.map(({ link }) => link);
+
         const schema = string().url().notOneOf(urlsList);
+
         schema
           .validate(state.form.url)
-          .then(() => getHttpContents(state.form.url))
-          .then((data) => {
-            const parsedRSS = parseRSS(data);
-            return parsedRSS;
+          .then(() => {
+            state.form.state = 'sending';
+
+            return getHttpContents(state.form.url);
           })
+          .then(parseRSS)
           .then((parsedRSS) => {
             const feedId = uniqueId();
+
             const feed = {
               id: feedId,
               title: parsedRSS.title,
               description: parsedRSS.description,
               link: state.form.url,
             };
+
             state.feeds.push(feed);
+
             addPosts(feedId, parsedRSS.items, state);
+
             state.form.url = '';
           })
           .catch((error) => {
@@ -173,10 +178,9 @@ export default () => {
       elements.modal.modalElement.addEventListener('show.bs.modal', (e) => {
         const postId = e.relatedTarget.getAttribute('data-id');
         const post = state.posts.find(({ id }) => postId === id);
-        if (!post) {
-          return;
-        }
+
         const { title, description, link } = post;
+
         state.seenIds.add(postId);
         state.modal = { title, description, link };
       });
--- src/render.js
+++ src/render.js
@@ -1,23 +1,20 @@
 const buildElement = (tagName, options = {}) => {
   const element = document.createElement(tagName);
   const { style, textContent } = options;
+
   if (style) {
-    if (Array.isArray(style)) {
-      element.classList.add(...style);
-    } else {
-      element.classList.add(style);
-    }
+    element.classList.add(...style);
   }
+
   if (textContent) {
     element.textContent = textContent;
   }
+
   return element;
 };
 
 const buildContainer = (title, listElems) => {
-  const cardBorder = buildElement('div', {
-    style: ['card', 'border-0'],
-  });
+  const cardBorder = buildElement('div', { style: ['card', 'border-0'] });
   const cardBody = buildElement('div', { style: 'card-body' });
   const cardTitle = buildElement('h2', {
     style: ['card-title', 'h4'],
@@ -26,39 +23,45 @@ const buildContainer = (title, listElems) => {
   const list = buildElement('ul', {
     style: ['list-group', 'border-0', 'rounded-0'],
   });
+
   list.append(...listElems);
   cardBody.append(cardTitle);
   cardBorder.append(cardBody, list);
+
   return cardBorder;
 };
 
-const handleFormState = ({ submit, input }, formState, i18nInstance) => {
+const handleFormState = (elements, formState, i18nInstance) => {
   switch (formState) {
     case 'filling':
-      submit.disabled = false;
-      submit.textContent = i18nInstance.t('form.submit');
-      input.focus();
+      elements.submit.disabled = false;
+      elements.submit.textContent = i18nInstance.t('form.submit');
+      elements.input.focus();
       break;
+
     case 'sending':
-      submit.disabled = true;
-      submit.textContent = i18nInstance.t('form.loading');
+      elements.submit.disabled = true;
+      elements.submit.textContent = i18nInstance.t('form.loading');
       break;
+
     default:
       throw new Error(`Unexpected form state: ${formState}`);
   }
 };
 
-const handleErrors = ({ input, feedback }, error, i18nInstance) => {
-  feedback.classList.remove('text-success');
-  feedback.classList.add('text-danger');
+const handleErrors = (elements, error, i18nInstance) => {
+  elements.feedback.classList.remove('text-success');
+  elements.feedback.classList.add('text-danger');
+
   if (error === '') {
-    input.classList.remove('is-invalid');
-    feedback.textContent = '';
+    elements.input.classList.remove('is-invalid');
+    elements.feedback.textContent = '';
     return;
   }
-  input.classList.add('is-invalid');
-  feedback.textContent = i18nInstance.t(`errors.${error}`);
-  input.focus();
+
+  elements.input.classList.add('is-invalid');
+  elements.feedback.textContent = i18nInstance.t(`errors.${error}`);
+  elements.input.focus();
 };
 
 const handleFeeds = (container, feeds, i18nInstance) => {
@@ -66,19 +69,25 @@ const handleFeeds = (container, feeds, i18nInstance) => {
     const listElem = buildElement('li', {
       style: ['list-group-item', 'border-0', 'border-end-0'],
     });
+
     const titleElem = buildElement('h3', {
       style: ['h6', 'm-0'],
       textContent: title,
     });
+
     const descriptionElem = buildElement('p', {
       style: ['m-0', 'small', 'text-black-50'],
       textContent: description,
     });
+
     listElem.append(titleElem, descriptionElem);
+
     return listElem;
   });
-  const containerTitle = i18nInstance.t('feeds');
-  const feedsContainer = buildContainer(containerTitle, listElems);
+
+  const title = i18nInstance.t('feeds');
+  const feedsContainer = buildContainer(title, listElems);
+
   container.replaceChildren(feedsContainer);
 };
 
@@ -90,65 +99,77 @@ const handlePosts = (container, posts, seenIds, i18nInstance) => {
         'd-flex',
         'justify-content-between',
         'align-items-baseline',
-        'border-end-0',
+        'border-end-g',
       ],
     });
+
     const linkElem = buildElement('a', {
       style: seenIds.has(id) ? ['fw-normal'] : ['fw-bold'],
       textContent: title,
     });
+
     linkElem.href = link;
     linkElem.target = '_blank';
     linkElem.rel = 'noopener noreferrer';
     linkElem.setAttribute('data-id', id);
+
     const button = buildElement('button', {
       style: ['btn', 'btn-outline-primary', 'btn-sm'],
       textContent: i18nInstance.t('preview'),
     });
+
     button.type = 'button';
     button.setAttribute('data-bs-toggle', 'modal');
     button.setAttribute('data-bs-target', '#modal');
     button.setAttribute('data-id', id);
+
     listElem.append(linkElem, button);
+
     return listElem;
   });
-  const containerTitle = i18nInstance.t('posts');
-  const postsContainer = buildContainer(containerTitle, listElems);
+
+  const title = i18nInstance.t('posts');
+  const postsContainer = buildContainer(title, listElems);
+
   container.replaceChildren(postsContainer);
 };
 
-export default (elements, state, i18nInstance) => {
-  const { input, submit, feedback, feeds, posts, modal } = elements;
-  return (path, value) => {
-    switch (path) {
-      case 'form.url':
-        input.value = value;
-        break;
-      case 'form.state':
-        handleFormState({ submit, input }, value, i18nInstance);
-        break;
-      case 'form.error':
-        handleErrors({ input, feedback }, value, i18nInstance);
-        break;
-      case 'feeds':
-        feedback.classList.remove('text-danger');
-        feedback.classList.add('text-success');
-        feedback.textContent = i18nInstance.t('success');
-        handleFeeds(feeds, value, i18nInstance);
-        break;
-      case 'posts':
-        handlePosts(posts, value, state.seenIds, i18nInstance);
-        break;
-      case 'seenIds':
-        handlePosts(posts, state.posts, state.seenIds, i18nInstance);
-        break;
-      case 'modal':
-        modal.title.textContent = value.title;
-        modal.body.textContent = value.description;
-        modal.showFull.href = value.link;
-        break;
-      default:
-        throw new Error(`Unexpected state: ${path}`);
-    }
-  };
+export default (elements, state, i18nInstance) => (path, value) => {
+  switch (path) {
+    case 'form.url':
+      elements.input.value = value;
+      break;
+
+    case 'form.state':
+      handleFormState(elements, value, i18nInstance);
+      break;
+
+    case 'form.error':
+      handleErrors(elements, value, i18nInstance);
+      break;
+
+    case 'feeds':
+      elements.feedback.classList.remove('text-danger');
+      elements.feedback.classList.add('text-success');
+      elements.feedback.textContent = i18nInstance.t('success');
+      handleFeeds(elements.feeds, value, i18nInstance);
+      break;
+
+    case 'posts':
+      handlePosts(elements.posts, value, state.seenIds, i18nInstance);
+      break;
+
+    case 'seenIds':
+      handlePosts(elements.posts, state.posts, state.seenIds, i18nInstance);
+      break;
+
+    case 'modal':
+      elements.modal.title.textContent = value.title;
+      elements.modal.body.textContent = value.description;
+      elements.modal.showFull.href = value.link;
+      break;
+
+    default:
+      throw new Error(`Unexpected state: ${path}`);
+  }
 };
